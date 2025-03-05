b310b5ee78f79524b91c66279316663d2ceb859e
CHINTHOJU SHIVAKUMAR <shivakumarchinthoju@gmail.com>
2025-03-05 17:06:01 +0530

    user api completed

--- App.py
+++ App.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from Routes.user_route import auth_bp
 from models import initialize_db
 from config import Config
@@ -9,6 +9,15 @@ app = Flask(__name__)
 
 app.config.from_object(Config)
 initialize_db(app)
+
+@app.before_request
+def check_auth():
+    
+    open_routes = ["/api/register", "/api/login"]
+    if request.path not in open_routes:
+        auth_headers = request.headers.get("Authorization")
+        if not auth_headers:
+            return jsonify({"message": "unauthorized"}), 401
 
 app.register_blueprint(auth_bp, url_prefix="/api")
 
